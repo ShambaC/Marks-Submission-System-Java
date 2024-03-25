b12e0a9150f3b06a31eb2e9f911b2f1153ff5a9a
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-03-26 01:58:53 +0530

    Moved config to different package

---
+++ src/conf/config.java
@@ -0,0 +1,13 @@
+package conf;
+public class config {
+    // Env location
+    public static final String envFileLocation = "./src/.env";
+
+    // Database connection details
+    public static final String DBaddress = "localhost:3306";
+    public static final String DBUser = "root";
+    public static final String DBPassword = "";
+
+    // Databse schema details location
+    public static final String schemaLocation = "./src/sql/";
+}
