d53a3e83246870460a9c3dcc22af56a73983a6b1
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-03-26 12:41:15 +0530

    Update App

--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
+import utility.DButil;
 import view.LoginView;
+import conf.config;
 
 public class App {
     
     public static void main(String[] args) {
+        DButil dbUtil = new DButil(config.DBaddress, config.DBUser, config.DBPassword, config.schemaLocation);
+        dbUtil.dbInit();
+
         LoginView view = new LoginView();
         view.setVisible(true);
     }
